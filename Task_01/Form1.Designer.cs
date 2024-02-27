15c034a6df477072b17a685b3a7d878133cad92c
matve <matve@DESKTOP-GPMHDR2>
2024-02-27 23:52:28 +0300

    Добавьте файлы проекта.

---
+++ Task_01/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Task_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.MenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemLoading = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDraft = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemChange = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.TbxInput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.TbCon = new System.Windows.Forms.TabControl();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFile,
+            this.MenuItemDraft});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(763, 28);
+            this.MenuStrip.TabIndex = 0;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // MenuItemFile
+            // 
+            this.MenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemLoading,
+            this.MenuItemSave,
+            this.MenuItemClose});
+            this.MenuItemFile.Name = "MenuItemFile";
+            this.MenuItemFile.Size = new System.Drawing.Size(59, 24);
+            this.MenuItemFile.Text = "Файл";
+            // 
+            // MenuItemLoading
+            // 
+            this.MenuItemLoading.Name = "MenuItemLoading";
+            this.MenuItemLoading.Size = new System.Drawing.Size(166, 26);
+            this.MenuItemLoading.Text = "Загрузить";
+            this.MenuItemLoading.Click += new System.EventHandler(this.MenuItemLoading_Click);
+            // 
+            // MenuItemSave
+            // 
+            this.MenuItemSave.Name = "MenuItemSave";
+            this.MenuItemSave.Size = new System.Drawing.Size(166, 26);
+            this.MenuItemSave.Text = "Сохранить";
+            this.MenuItemSave.Click += new System.EventHandler(this.MenuItemSave_Click);
+            // 
+            // MenuItemClose
+            // 
+            this.MenuItemClose.Name = "MenuItemClose";
+            this.MenuItemClose.Size = new System.Drawing.Size(166, 26);
+            this.MenuItemClose.Text = "Выход";
+            this.MenuItemClose.Click += new System.EventHandler(this.MenuItemClose_Click);
+            // 
+            // MenuItemDraft
+            // 
+            this.MenuItemDraft.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemAdd,
+            this.MenuItemChange,
+            this.MenuItemDelete});
+            this.MenuItemDraft.Name = "MenuItemDraft";
+            this.MenuItemDraft.Size = new System.Drawing.Size(125, 24);
+            this.MenuItemDraft.Text = "Редактировать";
+            // 
+            // MenuItemAdd
+            // 
+            this.MenuItemAdd.Name = "MenuItemAdd";
+            this.MenuItemAdd.Size = new System.Drawing.Size(161, 26);
+            this.MenuItemAdd.Text = "Добавить";
+            this.MenuItemAdd.Click += new System.EventHandler(this.MenuItemAdd_Click);
+            // 
+            // MenuItemChange
+            // 
+            this.MenuItemChange.Name = "MenuItemChange";
+            this.MenuItemChange.Size = new System.Drawing.Size(161, 26);
+            this.MenuItemChange.Text = "Изменить";
+            this.MenuItemChange.Click += new System.EventHandler(this.MenuItemChange_Click);
+            // 
+            // MenuItemDelete
+            // 
+            this.MenuItemDelete.Name = "MenuItemDelete";
+            this.MenuItemDelete.Size = new System.Drawing.Size(161, 26);
+            this.MenuItemDelete.Text = "Удалить";
+            this.MenuItemDelete.Click += new System.EventHandler(this.MenuItemDelete_Click);
+            // 
+            // TbxInput
+            // 
+            this.TbxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TbxInput.Location = new System.Drawing.Point(16, 340);
+            this.TbxInput.Name = "TbxInput";
+            this.TbxInput.Size = new System.Drawing.Size(727, 22);
+            this.TbxInput.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // TbCon
+            // 
+            this.TbCon.Location = new System.Drawing.Point(16, 48);
+            this.TbCon.Name = "TbCon";
+            this.TbCon.SelectedIndex = 0;
+            this.TbCon.Size = new System.Drawing.Size(727, 269);
+            this.TbCon.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 388);
+            this.Controls.Add(this.TbCon);
+            this.Controls.Add(this.TbxInput);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form1";
+            this.Text = "WF_28_Блокнот";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemLoading;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemSave;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemClose;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDraft;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemAdd;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemChange;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDelete;
+        private System.Windows.Forms.TextBox TbxInput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TabControl TbCon;
+    }
+}
+
---
+++ Task_01/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_01
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItemLoading_Click(object sender, EventArgs e)
+        {
+           openFileDialog1.FileName = "demo.txt";
+           if (openFileDialog1.ShowDialog() == DialogResult.OK)
+           {
+                string[] fileString = File.ReadAllLines(openFileDialog1.FileName);
+                int index = 0;
+                ListBox listDerma = (ListBox) TbCon.TabPages[index].Controls[0];
+                listDerma.Items.Clear();
+                foreach (string item in fileString)
+                {
+                    if (item == "#")
+                    {
+                        index++;
+                        listDerma = (ListBox) TbCon.TabPages[index].Controls[0];
+                        listDerma.Items.Clear();
+                    }
+                    else 
+                    {
+                        listDerma.Items.Add(item);
+                    }
+                }
+           }
+        }
+
+        private void MenuItemSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "demo.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                string filePath = saveFileDialog1.FileName;
+                File.WriteAllText(filePath, "");
+                int index = 0;
+                foreach (TabPage item in TbCon.TabPages)
+                { 
+                    ListBox list = (ListBox) item.Controls[0];
+                    string[] clist = list.Items.OfType<string>().ToArray();
+                    File.AppendAllLines(filePath, clist);
+                    if (index < 11) File.AppendAllText(filePath, "#\r\n");
+                    index++;
+                }
+                MessageBox.Show("Сохранение успешно!");
+            }
+        }
+
+        private void MenuItemAdd_Click(object sender, EventArgs e)
+        {
+            ListBox list = (ListBox) TbCon.SelectedTab.Controls[0];
+            list.Items.Add(TbxInput.Text);
+            TbxInput.Text = "";
+        }
+
+        private void MenuItemDelete_Click(object sender, EventArgs e)
+        {
+            ListBox list = (ListBox) TbCon.SelectedTab.Controls[0];
+            list.Items.RemoveAt(list.SelectedIndex);
+        }
+
+        private void MenuItemChange_Click(object sender, EventArgs e)
+        {
+           
+           ListBox list = (ListBox) TbCon.SelectedTab.Controls[0];
+           list.Items[list.SelectedIndex] = TbxInput.Text;
+           TbxInput.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> Dermo = new List<string> {"Январь","Февраля","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+            foreach (string item in Dermo)
+            {
+                TbCon.TabPages.Add(item);
+                TbCon.TabPages[TbCon.TabCount - 1].Controls.Add(new ListBox());
+                TbCon.TabPages[TbCon.TabCount - 1].Controls[0].Size = new Size(TbCon.Width - 10, TbCon.Height - 10);
+            }
+        }
+    }
+}
